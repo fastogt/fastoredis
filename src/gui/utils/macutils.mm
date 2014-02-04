8d8466ddd0469b21f468a42da4e0864388fb162a
topilski <atopilski@neosphere.com>
2014-02-04 09:12:24 +0400

    Added mac utils sources

---
+++ src/gui/utils/macutils.mm
@@ -0,0 +1,39 @@
+#include "macutils.h"
+#include <Cocoa/Cocoa.h>
+
+namespace
+{
+    bool isLion()
+    {
+        NSString *string = [NSString string];
+        // this selector was added only in Lion. so we can check if it's responding, we are on Lion
+        return [string respondsToSelector:@selector(linguisticTagsInRange:scheme:options:orthography:tokenRanges:)];
+    }
+}
+
+namespace nptv
+{
+    void addFullscreenButton(QWidget* widget)
+    {
+#if defined(MAC_OS_X_VERSION_10_7) && MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_7
+    if (isLion()) // checks if lion is running
+    {
+        const WId id = widget->winId();
+        NSView *nsview = reinterpret_cast<NSView*>(id);
+        NSWindow *nswindow = [nsview window];
+        [nswindow setCollectionBehavior:NSWindowCollectionBehaviorFullScreenPrimary];
+    }
+#else
+    #warning no fullscreen support will be included in this build
+#endif
+    }
+
+    void gotoFullScreen(QWidget *widget)
+    {
+        const WId id = widget->winId();
+        NSView *nsview = reinterpret_cast<NSView*>(id);
+        NSWindow *nswindow = [nsview window];
+        [nswindow toggleFullScreen:nil];
+    }
+}
+
