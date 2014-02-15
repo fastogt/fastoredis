eb1b5279ae08d3c99f81dcff416ca2fac77b4452
topilski <topilski@github.com>
2014-02-15 17:25:51 +0400

    Aded getMacAddress function for window

--- src/common/utils.cpp
+++ src/common/utils.cpp
@@ -1,4 +1,8 @@
 #include "common/utils.h"
+#ifdef OS_WIN
+#include <windows.h>
+#include <iphlpapi.h>
+#endif
 #ifdef OS_POSIX
 #include <signal.h>
 namespace common
@@ -47,4 +51,73 @@ namespace common
     }
 }
 #endif
+namespace common
+{
+    namespace utils
+    {
+        namespace net
+        {
+            namespace details
+            {
+                std::string get_mac_address_host_impl(IPAddr destination, IPAddr source)
+                {
+                    std::string result;
+                    ULONG MacAddr[2];       /* for 6-byte hardware addresses */
+                    ULONG PhysAddrLen = 6;  /* default to length of six bytes */
 
+                    DWORD dwRetVal = SendARP(destination, source, reinterpret_cast<PULONG>(&MacAddr), &PhysAddrLen);
+
+                    if (dwRetVal == NO_ERROR) {
+                        BYTE *bPhysAddr = (BYTE *)&MacAddr;
+                        for (int i = 0; i < PhysAddrLen; i++) {
+                            char tmp[4] = {0};
+                            if (i == (PhysAddrLen - 1))
+                                sprintf(tmp, "%.2X", bPhysAddr[i]);
+                            else
+                                sprintf(tmp, "%.2X-", bPhysAddr[i]);
+
+                            result += tmp;
+                        }
+                    }
+                    else if(dwRetVal == ERROR_BAD_NET_NAME){
+
+                    }
+                    else if(dwRetVal == ERROR_BUFFER_OVERFLOW){
+
+                    }
+                    else if(dwRetVal == ERROR_GEN_FAILURE){
+
+                    }
+                    else if(dwRetVal == ERROR_INVALID_PARAMETER){
+
+                    }
+                    else if(dwRetVal == ERROR_INVALID_USER_BUFFER){
+
+                    }
+                    else if(dwRetVal == ERROR_NOT_FOUND){
+
+                    }
+                    else if(dwRetVal == ERROR_NOT_SUPPORTED){
+
+                    }
+
+                    return result;
+                }
+            }
+            std::string get_mac_address_host(const std::string &host)
+            {
+                hostent * record = gethostbyname(host.c_str());
+                std::string result;
+                if(record == NULL)
+                {
+                    return result;
+                }
+                in_addr * address = (in_addr * )record->h_addr;
+                const char* ip_address = inet_ntoa(* address);
+                IPAddr DestIp = inet_addr(ip_address);
+                result = details::get_mac_address_host_impl(DestIp, INADDR_ANY);
+                return result;
+            }
+        }
+    }
+}
