4e70c390351e490fdcff662df753e319887850fb
topilski <atopilski@gmail.com>
2014-11-23 10:17:03 +0300

    Start size fixed

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,19 @@
 
 int main(int argc, char *argv[])
 {
+    const QSize prefSize(1024, 768);
     QApplication app(argc, argv);
     QRect screenGeometry = app.desktop()->availableGeometry();
+    const QSize screenSize(screenGeometry.width(), screenGeometry.height());
     QSize size(screenGeometry.width()/2, screenGeometry.height()/2);
 
     fastoredis::MainWindow win;
-    win.resize(size);
+    if(prefSize.height() < screenSize.height() && prefSize.width() < screenSize.width()){
+        win.resize(prefSize);
+    }
+    else{
+        win.resize(size);
+    }
 
     QPoint center = screenGeometry.center();
     win.move(center.x() - win.width() * 0.5, center.y() - win.height() * 0.5);
