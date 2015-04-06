d38edcc097f730419870e1c05a58b52b9dcb2397
topilski <atopilski@gmail.com>
2015-04-06 20:33:46 +0300

    Fix Windows build

--- src/core/redis/redis_driver.cpp
+++ src/core/redis/redis_driver.cpp
@@ -1,5 +1,26 @@
 #include "core/redis/redis_driver.h"
 
+#ifdef __MINGW32__
+    char *strcasestr(const char* s, const char* find)
+    {
+        char c, sc;
+        size_t len;
+
+        if ((c = *find++) != 0) {
+            c = tolower((unsigned char)c);
+            len = strlen(find);
+            do {
+                do {
+                    if ((sc = *s++) == 0)
+                        return (NULL);
+                } while ((char)tolower((unsigned char)sc) != c);
+            } while (strncasecmp(s, find, len) != 0);
+            s--;
+        }
+        return ((char *)s);
+    }
+#endif
+
 #include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
